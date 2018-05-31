5b2dc1fc077fe1b3a53e44ab39758d6becf53c2b
kanekotic <alvarojosepl@gmail.com>
2018-05-31 09:02:16 +0200

    csv parsing added

---
+++ test/lib/parsers/csv-parser-test.js
@@ -0,0 +1,42 @@
+jest.mock('fs', () => {
+    return {
+        createReadStream: jest.fn()
+    }
+})
+
+const fs = require('fs'),
+    parser = require('../../../lib/parsers/csv-parser'),
+    faker = require('faker'),
+    readableStream = require('stream').Readable,
+    writableStream = require('stream').Writable
+
+let streamFromArray = (array) => {
+    let stream = new readableStream
+    array.forEach(element => stream.push(element))
+    stream.push(null)
+    return stream
+}
+
+describe('csv parse', () => {
+
+    beforeEach(() => {
+        fs.createReadStream.mockClear()
+        let stream = streamFromArray(["1234,2345\n","4444,5555\n", null])
+        fs.createReadStream.mockImplementation(() => stream)
+    })
+
+    it('returns promise', () => {
+        expect(parser('')).toBeInstanceOf(Promise)
+    })
+
+    it('parses pased file', () => {
+        let path = `/path/to/file/${faker.random.uuid()}.txt`
+        parser(path)
+        expect(fs.createReadStream.mock.calls[0][0]).toBe(path)
+    })
+
+    it('parses parses data based on passed delimiter', () => {
+        let result = parser('', ',')
+        return expect(result).resolves.toEqual([["1234","2345"],["4444","5555"]])
+    })
+})
